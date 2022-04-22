b496dc129a9d4475296e183ae5296001051f41d5
LukaFilipovic99 <74373893+LukaFilipovic99@users.noreply.github.com>
2022-04-22 10:54:08 +0200

    Project configured using Spring Initializr

---
+++ src/main/java/com/lukafilipovic/PancakesUnlimitedApp/PancakesUnlimitedAppApplication.java
@@ -0,0 +1,13 @@
+package com.lukafilipovic.PancakesUnlimitedApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PancakesUnlimitedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PancakesUnlimitedAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lukafilipovic/PancakesUnlimitedApp/PancakesUnlimitedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukafilipovic.PancakesUnlimitedApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PancakesUnlimitedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
